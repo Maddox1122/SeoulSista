7841404cbdfaec65115ef736abaa501bfd8bcd5a
Maddox de Rooij <89667@glr.nl>
2023-03-09 11:11:14 +0100

    Home afgemaakt en begonnen aan Bestel pagina

--- JS/Hamburgermenu.js
+++
@@ -1,8 +0,0 @@
-const menu = document.querySelector(".hamburger");
-const nav = document.querySelector(".navlist");
-
-menu.addEventListener('click', () => {
-    nav.classList.remove(".max-[430px]:hidden")
-    nav.classList.add(".block")
-    console.log("hallo")
-})
