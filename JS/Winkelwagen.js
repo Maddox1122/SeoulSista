0a7cdce3ecc3992b484c65b6c704c6a642366324
Maddox de Rooij <89667@glr.nl>
2023-03-15 18:12:49 +0100

    Overlay knoppen werkend gemaakt

--- JS/Winkelwagen.js
+++ JS/Winkelwagen.js
@@ -1,3 +1,30 @@
-const cart = document.querySelector(".cart");
+const openOverlayButtons = document.querySelectorAll('.openOverlay');
+const annuleren = document.querySelectorAll('.annuleren');
+const confirm = document.querySelectorAll('.confirmtext');
 
-cart.addEventListener('click')
+openOverlayButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const targetOverlayId = button.getAttribute('data-target');
+    const targetOverlay = document.querySelector(targetOverlayId);
+
+    targetOverlay.classList.remove('hidden');
+  });
+});
+
+annuleren.forEach((button) => {
+    button.addEventListener('click', () => {
+        const annulerenId = button.getAttribute('data-target');
+        const annulerenBestel = document.querySelector(annulerenId);
+
+        annulerenBestel.classList.add('hidden');
+    })
+})
+
+// confirm.forEach((button) => {
+//     button.addEventListener('click', () => {
+//         const Bestel = button.getAttribute('data-target');
+//         const Bestellen = button.getAttribute(Bestel);
+
+
+//     })
+// })
