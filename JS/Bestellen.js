64bb096545cf8c95383d9bbb897d5bd672a0ef8f
Maddox de Rooij <89667@glr.nl>
2023-03-17 14:51:18 +0100

    Bestel verder gemaakt

--- JS/Bestellen.js
+++ JS/Bestellen.js
@@ -1,44 +1,58 @@
-const openOverlayButtons = document.querySelectorAll('.openOverlay');
-const annuleren = document.querySelectorAll('.annuleren');
-const confirm = document.querySelectorAll('.confirmtext');
-let aantalInputs = document.querySelectorAll(".aantal");
-let prijsElementen = document.querySelectorAll(".Prijs");
-const prijzen = [21.99, 14.99, 17.99, 19.99, 24.99, 34.99, 28.99, 9.99];
+const OpenOverlayButtons = document.querySelectorAll(".openOverlay");
+const Annuleren = document.querySelectorAll(".annuleren");
+const Confirm = document.querySelectorAll(".confirmtext");
+let AantalInputs = document.querySelectorAll(".aantal");
+let PrijsElementen = document.querySelectorAll(".Prijs");
+const Prijzen = [21.99, 14.99, 17.99, 19.99, 24.99, 34.99, 28.99, 9.99];
+const Toevoegen = document.querySelectorAll(".toevoegen");
+const Verwijderen = document.querySelectorAll(".verwijderen");
+const Winkelmandje = document.querySelector(".winkelmandje");
 
+OpenOverlayButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const TargetOverlayId = button.getAttribute("data-target");
+    const TargetOverlay = document.querySelector(TargetOverlayId);
 
-openOverlayButtons.forEach((button) => {
-  button.addEventListener('click', () => {
-    const targetOverlayId = button.getAttribute('data-target');
-    const targetOverlay = document.querySelector(targetOverlayId);
-
-    targetOverlay.classList.remove('hidden');
+    TargetOverlay.classList.remove("hidden");
   });
 });
 
-annuleren.forEach((button) => {
-    button.addEventListener('click', () => {
-        const annulerenId = button.getAttribute('data-target');
-        const annulerenBestel = document.querySelector(annulerenId);
+Annuleren.forEach((button) => {
+  button.addEventListener("click", () => {
+    const AnnulerenId = button.getAttribute("data-target");
+    const AnnulerenBestel = document.querySelector(AnnulerenId);
 
-        annulerenBestel.classList.add('hidden');
-    })
-})
-
-aantalInputs.forEach(function(aantalInput, index) {
-  aantalInput.addEventListener("input", function() {
-    let aantal = aantalInput.value;
-
-    let nieuwePrijs = prijzen[index] * aantal;
-
-    prijsElementen[index].textContent = "Prijs: €" + nieuwePrijs.toFixed(2) + "PP";
+    AnnulerenBestel.classList.add("hidden");
   });
 });
 
-// confirm.forEach((button) => {
-//     button.addEventListener('click', () => {
-//         const Bestel = button.getAttribute('data-target');
-//         const Bestellen = button.getAttribute(Bestel);
+AantalInputs.forEach(function (aantalInput, index) {
+  aantalInput.addEventListener("input", function () {
+    let Aantal = aantalInput.value;
 
+    let NieuwePrijs = Prijzen[index] * Aantal;
 
-//     })
-// })
+    PrijsElementen[index].textContent =
+      "Prijs: €" + NieuwePrijs.toFixed(2) + "PP";
+  });
+});
+
+Toevoegen.forEach((button) => {
+  button.addEventListener("click", () => {
+    const AnnulerenId = button.getAttribute("data-target");
+    const AnnulerenBestel = document.querySelector(AnnulerenId);
+
+    AnnulerenBestel.classList.add("hidden");
+  });
+});
+
+Verwijderen.forEach((button) => {
+  button.addEventListener("click", () => {
+    const AnnulerenId = button.getAttribute("data-target");
+    const AnnulerenBestel = document.querySelector(AnnulerenId);
+
+    AnnulerenBestel.classList.add("hidden");
+  });
+});
+
+Winkelmandje.addEventListener("click", () => {});
